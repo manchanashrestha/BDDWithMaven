98566ba96ad0ae63e318d88f87a5520ebaff7477
Manchana <sthamanchana@gmail.com>
2024-01-17 11:46:51 -0600

    Implemented Hooks

--- BDDWithMaven/src/test/java/features/EappLogin.feature
+++ BDDWithMaven/src/test/java/features/EappLogin.feature
@@ -8,11 +8,11 @@ Scenario: Validate login with correct credential
 	And    Locate log in button and click on it
 	Then   Validate if user is able to login or not.
 	
-Scenario: Validate login with correct credential
+Scenario: Validate login with incorrect userid and password
 	Given  Init the Browser and Navigate "http://eaapp.somee.com/"
 	Then   Click on login
-	And    Locate User Textbox and Input UserName as "admin1"
-	And    Locate Password TextBox and Input Password as "password"
+	And    Locate User Textbox and Input UserName as "abc"
+	And    Locate Password TextBox and Input Password as "xyz"
 	And    Locate log in button and click on it
-	Then   Validate if user is able to login or not.
+	Then   User should not allowed to login.
 	
--- BDDWithMaven/src/test/java/stepDefinition/Login.java
+++ BDDWithMaven/src/test/java/stepDefinition/Login.java
@@ -1,12 +1,18 @@
 package stepDefinition;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
+
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
@@ -15,40 +21,57 @@ public class Login {
 
 	@Given("Init the Browser and Navigate {string}")
 	public void init_the_browser_and_navigate(String url) {
-	    driver = new ChromeDriver();
-	    driver.manage().window().maximize();
-	    driver.get(url);
+		driver.get(url);
 	}
+
 	@Then("Click on login")
 	public void click_on_login() {
-		driver.findElement(By.id("loginLink")).click();;
-		
-	  
+		driver.findElement(By.id("loginLink")).click();
+		;
+
 	}
 
 	@Then("Locate User Textbox and Input UserName as {string}")
 	public void locate_user_textbox_and_input_user_name_as(String username) {
 		driver.findElement(By.id("UserName")).sendKeys(username);
-	  
-	}
 
+	}
 
 	@Then("Locate Password TextBox and Input Password as {string}")
 	public void locate_password_text_box_and_input_password_as(String password) {
 		driver.findElement(By.id("Password")).sendKeys(password);
-	   
+
 	}
 
 	@Then("Locate log in button and click on it")
 	public void locate_log_in_button_and_click_on_it() {
-		WebElement loginButton =  driver.findElement(By.xpath("//*[@id=\"loginForm\"]/form/div[4]/div/input"));
-		loginButton.click();
+		driver.findElement(By.xpath("//*[@id=\"loginForm\"]/form/div[4]/div/input")).click();
 	}
 
+	// Scenario 2 steops 5 and 6
 	@Then("Validate if user is able to login or not.")
 	public void validate_if_user_is_able_to_login_or_not() {
-	    WebElement IfLogin = driver.findElement(By.xpath("//*[@id=\"logoutForm\"]/ul/li[2]/a"));
-	    assertNotNull(IfLogin);
+		List<WebElement> IfLogin = driver.findElements(By.xpath("//*[@id=\"logoutForm\"]/ul/li[2]/a"));
+		IfLogin.get(0).click();
+		assertNotNull(IfLogin);
+	}
+
+	@Then("User should not allowed to login.")
+	public void user_should_not_allowed_to_login() {
+		List<WebElement> IfLogOffButtonExists = driver.findElements(By.xpath("//*[@id=\"logoutForm\"]/ul/li[2]/a"));
+		assertEquals(IfLogOffButtonExists.size(), 0);
+	}
+
+	@Before
+	public void SetUpBrowser() {
+
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+
+	@After
+	public void CloseBrowser() {
+		driver.close();
 	}
 
 }
